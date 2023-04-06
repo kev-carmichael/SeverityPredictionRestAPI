a4e948189f933dc347b5571956bc78d8628bccd0
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-06 13:33:53 +0100

    added simulation/SimulationRepository

--- RestApp/src/main/java/com/kev/RestApp/simulation/SimulationService.java
+++ RestApp/src/main/java/com/kev/RestApp/simulation/SimulationService.java
@@ -2,10 +2,14 @@ package com.kev.RestApp.simulation;
 
 import com.kev.RestApp.entity.Simulation;
 import com.kev.RestApp.factory.DTOFactory;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class SimulationService {
 
     private DTOFactory dtoFactory;
