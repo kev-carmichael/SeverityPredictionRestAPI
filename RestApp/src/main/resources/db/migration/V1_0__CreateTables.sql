2c47d145ecbdc4cbcd7070d8c4f8594332f15b74
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-06 14:03:33 +0100

    added flyway migration and generated request for view all simulations

--- RestApp/src/main/resources/db/migration/V1_0__CreateTables.sql
+++ RestApp/src/main/resources/db/migration/V1_0__CreateTables.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS simulation;
 
 CREATE TABLE user
 (
@@ -6,8 +7,19 @@ CREATE TABLE user
     email varchar(50) not null
 );
 
+CREATE TABLE simulation
+(
+    simulation_id integer primary key auto_increment,
+    last_input datetime not null
+);
+
 INSERT INTO user (email)
 VALUES ('adam@gmail.com'),
        ('bob@gmail.com'),
        ('colin@gmail.com');
+
+INSERT INTO simulation (last_input)
+VALUES ('2023-01-01 01:11:11'),
+       ('2023-02-02 02:22:22'),
+       ('2023-03-03 03:33:33');
 
