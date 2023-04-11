684008396106255a3adcf10542b5e785fb8b126f
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-11 11:48:32 +0100

    added WebApp/DTO/SimulationResult class

---
+++ WebApp/src/main/java/com/kev/WebApp/DTO/SimulationResult.java
@@ -0,0 +1,19 @@
+package com.kev.WebApp.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+
+public class SimulationResult
+{
+    private String simulationInput;
+    private String results;
+    
+}
