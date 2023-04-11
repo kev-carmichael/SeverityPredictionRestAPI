2df7a9082eaa2d8ce4c20ce913d04eeff1354e38
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-11 13:48:35 +0100

    web app: added (exclude = {DataSourceAutoConfiguration.class }) to @SpringBootApplication annotation

--- WebApp/src/main/java/com/kev/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/com/kev/WebApp/WebAppApplication.java
@@ -2,8 +2,9 @@ package com.kev.WebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WebAppApplication {
 
 	public static void main(String[] args) {
