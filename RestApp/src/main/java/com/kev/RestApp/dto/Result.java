df6b7b0093cd44196d460b90fad153e674850bd3
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-11 13:23:58 +0100

    added RestApp/dto/Result class

---
+++ RestApp/src/main/java/com/kev/RestApp/dto/Result.java
@@ -0,0 +1,18 @@
+package com.kev.RestApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Result
+{
+
+    private String results;
+
+}
