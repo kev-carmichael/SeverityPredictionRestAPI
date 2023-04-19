b7611442f8ade08e25c7ca0b8213f10fe0e54b0c
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-19 18:54:09 +0100

    validations added to UserDTO and controller

--- RestAPI/src/main/java/com/kev/RestAPI/user/UserDTO.java
+++ RestAPI/src/main/java/com/kev/RestAPI/user/UserDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+
 @AllArgsConstructor
 //@RequiredArgsConstructor - NEED TO UNCOMMENT THIS IF ADD MORE THAN 2 FIELDS
 @Getter
@@ -12,6 +15,9 @@ import lombok.Setter;
 
 public class UserDTO {
 
+    @Min(value = 1, message = "Id must be greater than 0")
     private final int userId;
+
+    @Email(message = "Email is not in the correct format")
     private final String email;
 }
