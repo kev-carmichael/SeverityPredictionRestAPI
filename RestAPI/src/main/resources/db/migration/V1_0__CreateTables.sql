95b3a9ab813fd28ecadc4decaf22a29f4101368e
Kev Carmichael <kev.carmichael22@gmail.com>
2023-04-21 10:51:19 +0100

    added text to severity and probability outputs in simulation results

--- RestAPI/src/main/resources/db/migration/V1_0__CreateTables.sql
+++ RestAPI/src/main/resources/db/migration/V1_0__CreateTables.sql
@@ -25,9 +25,9 @@ CREATE TABLE simulation
     ninety_day_hrs integer not null,
     twenty_eight_day_hrs integer not null,
     day_of_week varchar(10),
-    accident_severity varchar(50),
-    injury_severity varchar(50),
-    probability varchar(50)
+    accident_severity varchar(150),
+    injury_severity varchar(150),
+    probability varchar(150)
 );
 
 CREATE TABLE admin
@@ -44,9 +44,9 @@ VALUES ('adam@gmail.com', 'password1'),
        ('colin@gmail.com', 'password3');
 
 INSERT INTO simulation (last_input, user, age_aircraft, no_of_passengers, pic_licence, pic_age, total_hrs, type_hrs, ninety_day_hrs, twenty_eight_day_hrs, day_of_week, accident_severity, injury_severity, probability)
-VALUES ('2023-01-01', 1, 15, 0, 'student', 21, 35, 35, 24, 8, null, 'non-severe', null, '0.64'),
-       ('2023-02-02', 2, 25, 1, 'airline transport pilot licence', 52, 10000, 2, 180, 60, 'Wed', null, 'minor/none', '0.71'),
-       ('2023-03-03', 3, 35, 1, 'private pilot licence', 67, 3000, 2100, 24, 8, null, 'non-severe', null, '0.74');
+VALUES ('2023-01-01', 1, 15, 0, 'student', 21, 35, 35, 24, 8, null, 'Predicted severity class for simulation: non-severe', null, 'Estimated membership probability of the simulation for predicted severity class: 0.64'),
+       ('2023-02-02', 2, 25, 1, 'airline transport pilot licence', 52, 10000, 2, 180, 60, 'Wed', null, 'Predicted severity class for simulation: minor/none', 'Estimated membership probability of the simulation for predicted severity class: 0.71'),
+       ('2023-03-03', 3, 35, 1, 'private pilot licence', 67, 3000, 2100, 24, 8, null, 'Predicted severity class for simulation: non-severe', null, 'Estimated membership probability of the simulation for predicted severity class: 0.74');
 
 INSERT INTO admin(email, password)
 VALUES ('admin1@gmail.com', 'passwordadmin1'),
